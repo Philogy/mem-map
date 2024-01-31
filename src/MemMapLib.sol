42ffd893acddc6c20bc56b38e360549a5ee49eba
Philippe Dumonet <philippe@dumo.net>
2024-01-31 01:40:52 +0100

    ⚡️ Manually inline

--- src/MemMapLib.sol
+++ src/MemMapLib.sol
@@ -90,13 +90,66 @@ library MemMapLib {
     }
 
     function set(MemMap map, bytes32 key, uint256 value) internal pure {
-        (mapPairPtr ptr,) = map.getPairPtr(key);
-        ptr.set(value);
+        /// @solidity memory-safe-assembly
+        assembly {
+            let offsetMask := and(map, CAPACITY_MASK)
+            let mapPtr := shr(32, map)
+            let mapStart := add(mapPtr, 0x40)
+
+            let startOffset := and(key, offsetMask)
+            let keyOffset := startOffset
+            let keyPtr := add(mapStart, keyOffset)
+            let storedKey := mload(keyPtr)
+
+            // Search for key, best case: O(1), worst case: O(n).
+            for {} iszero(or(iszero(storedKey), eq(storedKey, key))) {} {
+                keyOffset := and(add(keyOffset, MAP_PAIR_SPACING), offsetMask)
+                keyPtr := add(mapStart, keyOffset)
+                storedKey := mload(keyPtr)
+                // Check if we wrapped around all the way to the start.
+                if eq(keyOffset, startOffset) {
+                    mstore(0x00, 0xda4685ce /* MapFull() */ )
+                    revert(0x1c, 0x04)
+                }
+            }
+
+            // Check if we're using a fresh slot in our map.
+            if iszero(storedKey) {
+                // Safe to decrement without checking because zero stored keys indicates empty map
+                // slots.
+                mstore(mapPtr, sub(mload(mapPtr), 1))
+                mstore(keyPtr, key)
+            }
+
+            mstore(sub(keyPtr, 0x20), value)
+        }
     }
 
     function get(MemMap map, bytes32 key) internal pure returns (uint256 value) {
-        (mapPairPtr ptr,) = map.getPairPtr(key);
-        value = ptr.get();
+        /// @solidity memory-safe-assembly
+        assembly {
+            let offsetMask := and(map, CAPACITY_MASK)
+            let mapPtr := shr(32, map)
+            let mapStart := add(mapPtr, 0x40)
+
+            let startOffset := and(key, offsetMask)
+            let keyOffset := startOffset
+            let keyPtr := add(mapStart, keyOffset)
+            let storedKey := mload(keyPtr)
+
+            // Search for key, best case: O(1), worst case: O(n).
+            for {} iszero(or(iszero(storedKey), eq(storedKey, key))) {} {
+                keyOffset := and(add(keyOffset, MAP_PAIR_SPACING), offsetMask)
+                keyPtr := add(mapStart, keyOffset)
+                storedKey := mload(keyPtr)
+                // Check if we wrapped around all the way to the start.
+                if eq(keyOffset, startOffset) {
+                    mstore(0x00, 0xda4685ce /* MapFull() */ )
+                    revert(0x1c, 0x04)
+                }
+            }
+            value := mload(sub(keyPtr, 0x20))
+        }
     }
 
     function isNull(MemMap map, bytes32 key) internal pure returns (bool) {
